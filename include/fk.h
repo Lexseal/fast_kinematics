4009f92fc129236dccc944d83fcba70f24b337d2
Xinsong Lin <zhongjiezhex12@me.com>
2024-02-08 16:01:27 -0800

    set up parallel framework

--- include/fk.h
+++ include/fk.h
@@ -8,12 +8,14 @@
 // data structure
 // [type (1 float), axis (3 floats), rotation (4 floats), translation (3 floats)]
 
-Eigen::Matrix<float, 7, 1> forward_kinematics(std::vector<float> data, std::vector<float> angs) {
+Eigen::Matrix<float, 7, 1> forward_kinematics(float* data, float* angs,
+  size_t num_of_joints, size_t num_of_active_joints, size_t idx) {
   Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
   Eigen::Quaternionf t = Eigen::Quaternionf(0, 0, 0, 0);
-  size_t num_of_joints = angs.size();
-  size_t idx = 0;
-  while (idx < data.size()) {
+  size_t end_idx = 11*num_of_joints*(idx+1);
+  size_t end_ang_idx = num_of_active_joints*(idx+1);
+  idx = num_of_joints*idx;
+  while (idx < end_idx) {
     float type = data[idx++];
     Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
     idx += 3;
@@ -21,21 +23,16 @@ Eigen::Matrix<float, 7, 1> forward_kinematics(std::vector<float> data, std::vect
     idx += 4;
     Eigen::Quaternionf translation = Eigen::Quaternionf(0, data[idx], data[idx + 1], data[idx + 2]);
     idx += 3;
-    std::cout << "type: " << type << std::endl;
     if (type == urdf::Joint::REVOLUTE) {
-      // convert Eigen::AngleAxisd(joint_values[--num_of_joints], walker->axis) to quaternion
-      Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[--num_of_joints], axis));
+      // convert Eigen::AngleAxisd(joint_values[--end_ang_idx], walker->axis) to quaternion
+      Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[--end_ang_idx], axis));
       r = change * r;
       t = change * t * change.inverse();
-      std::cout << change.coeffs().transpose() << std::endl;
     } else if (type == urdf::Joint::PRISMATIC) {
-      auto translation = angs[--num_of_joints] * axis;
+      auto translation = angs[--end_ang_idx] * axis;
       Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
       t.coeffs() += change.coeffs();
-      std::cout << change.coeffs().transpose() << std::endl;
     }
-    std::cout << rotation.coeffs().transpose() << std::endl;
-    std::cout << translation.coeffs().transpose() << std::endl;
     r = rotation * r;
     t = rotation * t * rotation.inverse();
     t.coeffs() += translation.coeffs();
--- tests/test_client.cpp
+++ tests/test_client.cpp
@@ -2,11 +2,21 @@
 #include <parser.h>
 #include <fk.h>
 
+size_t find_num_of_active_joints(JointTreePtr tip) {
+  size_t num_of_active_joints = 0;
+  JointTreePtr walker = tip;
+  while (walker) {
+    num_of_active_joints += (walker->type != urdf::Joint::FIXED);
+    walker = walker->parent;
+  }
+  return num_of_active_joints;
+}
+
 size_t find_num_of_joints(JointTreePtr tip) {
   size_t num_of_joints = 0;
   JointTreePtr walker = tip;
   while (walker) {
-    num_of_joints += (walker->type != urdf::Joint::FIXED);
+    ++num_of_joints;
     walker = walker->parent;
   }
   return num_of_joints;
@@ -47,53 +57,53 @@ int main() {
   Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
   Eigen::Quaternionf t = Eigen::Quaternionf(0, 0, 0, 0);
   JointTreePtr walker = tip;
-  size_t num_of_joints = find_num_of_joints(tip);
-  std::vector<float> joint_values{0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0.03};
-  while (walker) {
-    if (walker->type == urdf::Joint::REVOLUTE) {
-      // convert Eigen::AngleAxisf(joint_values[--num_of_joints], walker->axis) to quaternion
-      Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(joint_values[--num_of_joints], walker->axis));
-      r = change * r;
-      t = change * t * change.inverse();
-      std::cout << change.coeffs().transpose() << std::endl;
-    } else if (walker->type == urdf::Joint::PRISMATIC) {
-      auto translation = joint_values[--num_of_joints] * walker->axis;
-      Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
-      t.coeffs() += change.coeffs();
-      std::cout << change.coeffs().transpose() << std::endl;
-    }
-    std::cout << walker->rotation.coeffs().transpose() << std::endl;
-    std::cout << walker->position.coeffs().transpose() << std::endl;
-    r = walker->rotation * r;
-    t = walker->rotation * t * walker->rotation.inverse();
-    t.coeffs() += walker->position.coeffs();
-    walker = walker->parent;
-  }
-  std::cout << "The transformation from the tip to the root is: " << std::endl;
-  std::cout << "Rotation: " << r.coeffs().transpose() << std::endl;
-  std::cout << "Position: " << t.coeffs().transpose() << std::endl;
+  // size_t num_of_active_joints = find_num_of_active_joints(tip);
+  // std::vector<float> joint_values{0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0.03};
+  // while (walker) {
+  //   if (walker->type == urdf::Joint::REVOLUTE) {
+  //     // convert Eigen::AngleAxisf(joint_values[--num_of_active_joints], walker->axis) to quaternion
+  //     Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(joint_values[--num_of_active_joints], walker->axis));
+  //     r = change * r;
+  //     t = change * t * change.inverse();
+  //     std::cout << change.coeffs().transpose() << std::endl;
+  //   } else if (walker->type == urdf::Joint::PRISMATIC) {
+  //     auto translation = joint_values[--num_of_active_joints] * walker->axis;
+  //     Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
+  //     t.coeffs() += change.coeffs();
+  //     std::cout << change.coeffs().transpose() << std::endl;
+  //   }
+  //   std::cout << walker->rotation.coeffs().transpose() << std::endl;
+  //   std::cout << walker->position.coeffs().transpose() << std::endl;
+  //   r = walker->rotation * r;
+  //   t = walker->rotation * t * walker->rotation.inverse();
+  //   t.coeffs() += walker->position.coeffs();
+  //   walker = walker->parent;
+  // }
+  // std::cout << "The transformation from the tip to the root is: " << std::endl;
+  // std::cout << "Rotation: " << r.coeffs().transpose() << std::endl;
+  // std::cout << "Position: " << t.coeffs().transpose() << std::endl;
 
   // data structure
   // [type (1 float), axis (3 floats), rotation (4 floats), translation (3 floats)]
-  std::vector<float> data;
+  float *data = new float[11 * find_num_of_joints(tip)];
   walker = tip;
+  size_t idx = 0;
   while (walker) {
-    data.push_back(walker->type);
-    data.push_back(walker->axis.x());
-    data.push_back(walker->axis.y());
-    data.push_back(walker->axis.z());
-    data.push_back(walker->rotation.coeffs().w());
-    data.push_back(walker->rotation.coeffs().x());
-    data.push_back(walker->rotation.coeffs().y());
-    data.push_back(walker->rotation.coeffs().z());
-    data.push_back(walker->position.x());
-    data.push_back(walker->position.y());
-    data.push_back(walker->position.z());
+    data[idx++] = walker->type;
+    data[idx++] = walker->axis.x();
+    data[idx++] = walker->axis.y();
+    data[idx++] = walker->axis.z();
+    data[idx++] = walker->rotation.coeffs().w();
+    data[idx++] = walker->rotation.coeffs().x();
+    data[idx++] = walker->rotation.coeffs().y();
+    data[idx++] = walker->rotation.coeffs().z();
+    data[idx++] = walker->position.x();
+    data[idx++] = walker->position.y();
+    data[idx++] = walker->position.z();
     walker = walker->parent;
   }
-
-  // std::vector<float> joint_values{0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0.03};
-  std::cout << forward_kinematics(data, joint_values) << std::endl;
+  float angs[] = {0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0.03};
+  std::cout << forward_kinematics(data, angs, find_num_of_joints(tip), find_num_of_active_joints(tip), 0) << std::endl;
 
   return 0;
 }
