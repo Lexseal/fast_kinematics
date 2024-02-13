0af00679da2c20a0b3276b8ffa5ad1112c1a0dec
Xinsong Lin <zhongjiezhex12@me.com>
2024-02-12 16:49:00 -0800

    moved off eigen

---
+++ include/cuda_quat.h
@@ -0,0 +1,105 @@
+#pragma once
+
+#pragma once
+
+#include <cmath>
+
+class Quaternion {
+public:
+  /*__host__ __device__*/ Quaternion(float w = 1.0f, float x = 0.0f, float y = 0.0f, float z = 0.0f)
+    : w(w), x(x), y(y), z(z) {}
+
+  /*__host__ __device__*/ float norm() const {
+    return std::sqrt(w * w + x * x + y * y + z * z);
+  }
+
+  /*__host__ __device__*/ Quaternion conjugate() const {
+    return Quaternion(w, -x, -y, -z);
+  }
+
+  /*__host__ __device__*/ Quaternion operator+(const Quaternion& other) const {
+    return Quaternion(w + other.w, x + other.x, y + other.y, z + other.z);
+  }
+
+  /*__host__ __device__*/ Quaternion operator*(const Quaternion& other) const {
+    float nw = w * other.w - x * other.x - y * other.y - z * other.z;
+    float nx = w * other.x + x * other.w + y * other.z - z * other.y;
+    float ny = w * other.y - x * other.z + y * other.w + z * other.x;
+    float nz = w * other.z + x * other.y - y * other.x + z * other.w;
+    return Quaternion(nw, nx, ny, nz);
+  }
+
+  /*__host__ __device__*/ Quaternion operator*(float scalar) const {
+    return Quaternion(w * scalar, x * scalar, y * scalar, z * scalar);
+  }
+
+  /*__host__ __device__*/ Quaternion operator/(float scalar) const {
+    return Quaternion(w / scalar, x / scalar, y / scalar, z / scalar);
+  }
+
+  /*__host__ __device__*/ Quaternion& operator+=(const Quaternion& other) {
+    w += other.w;
+    x += other.x;
+    y += other.y;
+    z += other.z;
+    return *this;
+  }
+
+  /*__host__ __device__*/ Quaternion& operator*=(const Quaternion& other) {
+    float nw = w * other.w - x * other.x - y * other.y - z * other.z;
+    float nx = w * other.x + x * other.w + y * other.z - z * other.y;
+    float ny = w * other.y - x * other.z + y * other.w + z * other.x;
+    float nz = w * other.z + x * other.y - y * other.x + z * other.w;
+    w = nw;
+    x = nx;
+    y = ny;
+    z = nz;
+    return *this;
+  }
+
+  /*__host__ __device__*/ Quaternion& operator*=(float scalar) {
+    w *= scalar;
+    x *= scalar;
+    y *= scalar;
+    z *= scalar;
+    return *this;
+  }
+
+  /*__host__ __device__*/ Quaternion& operator/=(float scalar) {
+    w /= scalar;
+    x /= scalar;
+    y /= scalar;
+    z /= scalar;
+    return *this;
+  }
+
+  /*__host__ __device__*/ void normalize() {
+    float n = norm();
+    w /= n;
+    x /= n;
+    y /= n;
+    z /= n;
+  }
+
+  /*__host__ __device__*/ Quaternion normalized() const {
+    float n = norm();
+    return Quaternion(w / n, x / n, y / n, z / n);
+  }
+
+  /*__host__ __device__*/ Quaternion inverse() const {
+    return conjugate() / (w * w + x * x + y * y + z * z);
+  }
+
+  static Quaternion Identity() {
+    return Quaternion(1.0f, 0.0f, 0.0f, 0.0f);
+  }
+
+  static Quaternion FromAngleAxis(float angle, float x, float y, float z) {
+    float halfAngle = angle / 2;
+    float s = std::sin(halfAngle);
+    return Quaternion(std::cos(halfAngle), x * s, y * s, z * s);
+  }
+
+public:
+  float w, x, y, z;
+};
--- include/kinematics.h
+++ include/kinematics.h
@@ -1,125 +1,121 @@
 #pragma once
 
-#include <Eigen/Core>
-#include <Eigen/Geometry>
 #include <vector>
 #include <urdf_parser/urdf_parser.h>
+#include <cuda_quat.h>
 
 // data structure
 // [translation (3 floats), rotation (4 floats), type (1 float), axis (3 floats)]
 
-Eigen::Matrix<float, 7, 1> forward_kinematics(float *data, float *angs,
+float* forward_kinematics(float *data, float *angs,
   size_t *num_joints_cum, size_t *num_of_active_joints_cum, size_t idx) {
   size_t end_idx = num_joints_cum[idx];
   size_t ang_idx = idx == 0 ? 0 : num_of_active_joints_cum[idx - 1];
   if (idx > 0) idx = num_joints_cum[idx - 1];
-  Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
-  Eigen::Quaternionf t(0, data[idx], data[idx+1], data[idx+2]);
+  Quaternion r = Quaternion::Identity();
+  Quaternion t(0, data[idx], data[idx+1], data[idx+2]);
   idx += 3;
   while (idx < end_idx-11) {
-    Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
+    Quaternion rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
     idx += 4;
     float type = data[idx++];
-    Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
+    float axis[3] = {data[idx], data[idx + 1], data[idx + 2]};
     idx += 3;
-    Eigen::Quaternionf nxt_translation(0, data[idx], data[idx + 1], data[idx + 2]);
+    Quaternion nxt_translation(0, data[idx], data[idx + 1], data[idx + 2]);
     idx += 3;
 
     r = r * rotation;
     if (type == urdf::Joint::REVOLUTE) {
-      // convert Eigen::AngleAxisd(joint_values[ang_idx++], walker->axis) to quaternion
-      Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[ang_idx++], axis));
+      Quaternion change = Quaternion::FromAngleAxis(angs[ang_idx++], axis[0], axis[1], axis[2]);
       r = r * change;
     } else if (type == urdf::Joint::PRISMATIC) {
-      auto translation = angs[ang_idx++] * axis;
-      Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
-      nxt_translation.coeffs() += change.coeffs();
+      float displacement = angs[ang_idx++];
+      Quaternion change(0, displacement*axis[0], displacement*axis[1], displacement*axis[2]);
+      nxt_translation += change;
     }
     nxt_translation = r * nxt_translation * r.inverse();
-    t.coeffs() += nxt_translation.coeffs();
+    t += nxt_translation;
   }
 
-  Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
+  Quaternion rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
   idx += 4;
   float type = data[idx++];
-  Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
+  float axis[3] = {data[idx], data[idx + 1], data[idx + 2]};
   idx += 3;
-  Eigen::Quaternionf nxt_translation(0,0,0,0);
+  Quaternion nxt_translation(0,0,0,0);
 
   r = r * rotation;
   if (type == urdf::Joint::REVOLUTE) {
-    // convert Eigen::AngleAxisd(joint_values[ang_idx++], walker->axis) to quaternion
-    Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[ang_idx++], axis));
+    Quaternion change = Quaternion::FromAngleAxis(angs[ang_idx++], axis[0], axis[1], axis[2]);
     r = r * change;
   } else if (type == urdf::Joint::PRISMATIC) {
-    auto translation = angs[ang_idx++] * axis;
-    Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
-    nxt_translation.coeffs() += change.coeffs();
+    float displacement = angs[ang_idx++];
+    Quaternion change(0, displacement*axis[0], displacement*axis[1], displacement*axis[2]);
+    nxt_translation += change;
   }
   nxt_translation = r * nxt_translation * r.inverse();
-  t.coeffs() += nxt_translation.coeffs();
+  t += nxt_translation;
   
-  Eigen::Matrix<float, 7, 1> result;
-  result << t.x(), t.y(), t.z(), r.w(), r.x(), r.y(), r.z();
+  float *result = new float[7]{t.x, t.y, t.z, r.w, r.x, r.y, r.z};
   return result;
 }
 
-Eigen::MatrixXf jacobian(float *data, float *angs,
-  size_t *num_joints_cum, size_t *num_of_active_joints_cum, size_t idx) {
-  size_t end_idx = num_joints_cum[idx];
-  size_t start_ang_idx = idx == 0 ? 0 : num_of_active_joints_cum[idx - 1];
-  size_t ang_idx = start_ang_idx;
-  Eigen::MatrixXf result = Eigen::MatrixXf::Zero(6, num_of_active_joints_cum[idx] - ang_idx);
-  if (idx > 0) idx = num_joints_cum[idx - 1];  // idx is getting reused here!
-  Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
-  Eigen::Quaternionf t(0, data[idx], data[idx+1], data[idx+2]);
-  idx += 3;
-  while (idx < end_idx-11) {
-    Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
-    idx += 4;
-    float type = data[idx++];
-    Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
-    idx += 3;
-    Eigen::Quaternionf nxt_translation(0, data[idx], data[idx + 1], data[idx + 2]);
-    idx += 3;
+// Eigen::MatrixXf jacobian(float *data, float *angs,
+//   size_t *num_joints_cum, size_t *num_of_active_joints_cum, size_t idx) {
+//   size_t end_idx = num_joints_cum[idx];
+//   size_t start_ang_idx = idx == 0 ? 0 : num_of_active_joints_cum[idx - 1];
+//   size_t ang_idx = start_ang_idx;
+//   Eigen::MatrixXf result = Eigen::MatrixXf::Zero(6, num_of_active_joints_cum[idx] - ang_idx);
+//   if (idx > 0) idx = num_joints_cum[idx - 1];  // idx is getting reused here!
+//   Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
+//   Eigen::Quaternionf t(0, data[idx], data[idx+1], data[idx+2]);
+//   idx += 3;
+//   while (idx < end_idx-11) {
+//     Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
+//     idx += 4;
+//     float type = data[idx++];
+//     Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
+//     idx += 3;
+//     Eigen::Quaternionf nxt_translation(0, data[idx], data[idx + 1], data[idx + 2]);
+//     idx += 3;
 
-    r = r * rotation;
-    if (type == urdf::Joint::REVOLUTE) {
-      Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
-      axis_quat = r * axis_quat * r.inverse();
-      result.block(3, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
-      result.block(0, ang_idx-start_ang_idx, 3, 1) = -axis_quat.vec().cross(t.vec());
-      // convert Eigen::AngleAxisd(joint_values[ang_idx++], walker->axis) to quaternion
-      Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[ang_idx++], axis));
-      r = r * change;
-    } else if (type == urdf::Joint::PRISMATIC) {
-      Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
-      axis_quat = r * axis_quat * r.inverse();
-      result.block(0, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
-      Eigen::Vector3f translation = angs[ang_idx++] * axis;
-      Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
-      nxt_translation.coeffs() += change.coeffs();
-    }
-    nxt_translation = r * nxt_translation * r.inverse();
-    t.coeffs() += nxt_translation.coeffs();
-  }
+//     r = r * rotation;
+//     if (type == urdf::Joint::REVOLUTE) {
+//       Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
+//       axis_quat = r * axis_quat * r.inverse();
+//       result.block(3, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
+//       result.block(0, ang_idx-start_ang_idx, 3, 1) = -axis_quat.vec().cross(t.vec());
+//       // convert Eigen::AngleAxisd(joint_values[ang_idx++], walker->axis) to quaternion
+//       Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(angs[ang_idx++], axis));
+//       r = r * change;
+//     } else if (type == urdf::Joint::PRISMATIC) {
+//       Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
+//       axis_quat = r * axis_quat * r.inverse();
+//       result.block(0, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
+//       Eigen::Vector3f translation = angs[ang_idx++] * axis;
+//       Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
+//       nxt_translation.coeffs() += change.coeffs();
+//     }
+//     nxt_translation = r * nxt_translation * r.inverse();
+//     t.coeffs() += nxt_translation.coeffs();
+//   }
 
-  Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
-  idx += 4;
-  float type = data[idx++];
-  Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
-  idx += 3;
+//   Eigen::Quaternionf rotation(data[idx], data[idx + 1], data[idx + 2], data[idx + 3]);
+//   idx += 4;
+//   float type = data[idx++];
+//   Eigen::Vector3f axis = Eigen::Vector3f(data[idx], data[idx + 1], data[idx + 2]);
+//   idx += 3;
 
-  r = r * rotation;
-  if (type == urdf::Joint::REVOLUTE) {
-    Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
-    axis_quat = r * axis_quat * r.inverse();
-    result.block(3, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
-    result.block(0, ang_idx-start_ang_idx, 3, 1) = -axis_quat.vec().cross(t.vec());
-  } else if (type == urdf::Joint::PRISMATIC) {
-    Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
-    axis_quat = r * axis_quat * r.inverse();
-    result.block(0, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
-  }
-  return result;
-}
+//   r = r * rotation;
+//   if (type == urdf::Joint::REVOLUTE) {
+//     Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
+//     axis_quat = r * axis_quat * r.inverse();
+//     result.block(3, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
+//     result.block(0, ang_idx-start_ang_idx, 3, 1) = -axis_quat.vec().cross(t.vec());
+//   } else if (type == urdf::Joint::PRISMATIC) {
+//     Eigen::Quaternionf axis_quat(0, axis.x(), axis.y(), axis.z());
+//     axis_quat = r * axis_quat * r.inverse();
+//     result.block(0, ang_idx-start_ang_idx, 3, 1) = axis_quat.vec();
+//   }
+//   return result;
+// }
