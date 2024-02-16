3b3bd04257c7d8c24f9b3510cb762d2c46ee47b2
Xinsong Lin <x8lin@ucsd.edu>
2024-02-16 01:16:00 +0000

    profiling data

--- idea.md
+++ idea.md
@@ -34,3 +34,43 @@ two other arrays to specify cumulative number of joints for each robot and cumul
 ## Benchmarks
 
 The time it takes to calculate 10000 Jacobians in parallel (including the memory copy time but not including the data prepreation time) is ~2ms on m4000. The same calculation takes ~3.2ms on an M1 and ~9ms on E5-2623 single core.
+
+10000 J
+==18797== NVPROF is profiling process 18797, command: ./test_client
+==18797== Profiling application: ./test_client
+==18797== Profiling result:
+   Start  Duration            Grid Size      Block Size     Regs*    SSMem*    DSMem*      Size  Throughput  SrcMemType  DstMemType           Device   Context    Stream  Name
+352.26ms  619.91us                    -               -         -         -         -  4.1962MB  6.6103GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+352.93ms  33.407us                    -               -         -         -         -  312.50KB  8.9210GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+352.98ms  9.6000us                    -               -         -         -         -  78.125KB  7.7610GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+353.01ms  9.4080us                    -               -         -         -         -  78.125KB  7.9194GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+353.15ms  202.78us                    -               -         -         -         -  1.8311MB  8.8183GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+353.36ms  296.95us             (40 1 1)       (256 1 1)        48        0B        0B         -           -           -           -  Quadro M4000 (0         1         7  jacobian(float*, float*, unsigned long*, unsigned long*, float*, unsigned long) [122]
+353.66ms  194.49us                    -               -         -         -         -  1.8311MB  9.1940GB/s      Device    Pageable  Quadro M4000 (0         1         7  [CUDA memcpy DtoH]
+
+Regs: Number of registers used per CUDA thread. This number includes registers used internally by the CUDA driver and/or tools and can be more than what the compiler shows.
+SSMem: Static shared memory allocated per CUDA block.
+DSMem: Dynamic shared memory allocated per CUDA block.
+SrcMemType: The type of source memory accessed by memory operation/copy
+DstMemType: The type of destination memory accessed by memory operation/copy
+==18797== Generated result file: /home/paperspace/fast_kinematics/build/results.nvprof
+
+100000 J
+==19104== NVPROF is profiling process 19104, command: ./test_client
+==19104== Profiling application: ./test_client
+==19104== Profiling result:
+   Start  Duration            Grid Size      Block Size     Regs*    SSMem*    DSMem*      Size  Throughput  SrcMemType  DstMemType           Device   Context    Stream  Name
+311.65ms  5.8044ms                    -               -         -         -         -  41.962MB  7.0598GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+317.59ms  363.26us                    -               -         -         -         -  3.0518MB  8.2042GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+318.04ms  82.046us                    -               -         -         -         -  781.25KB  9.0810GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+318.22ms  81.438us                    -               -         -         -         -  781.25KB  9.1488GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+318.42ms  2.5035ms                    -               -         -         -         -  18.311MB  7.1424GB/s    Pageable      Device  Quadro M4000 (0         1         7  [CUDA memcpy HtoD]
+320.93ms  3.9205ms            (391 1 1)       (256 1 1)        48        0B        0B         -           -           -           -  Quadro M4000 (0         1         7  jacobian(float*, float*, unsigned long*, unsigned long*, float*, unsigned long) [122]
+324.86ms  2.0000ms                    -               -         -         -         -  18.311MB  8.9406GB/s      Device    Pageable  Quadro M4000 (0         1         7  [CUDA memcpy DtoH]
+
+Regs: Number of registers used per CUDA thread. This number includes registers used internally by the CUDA driver and/or tools and can be more than what the compiler shows.
+SSMem: Static shared memory allocated per CUDA block.
+DSMem: Dynamic shared memory allocated per CUDA block.
+SrcMemType: The type of source memory accessed by memory operation/copy
+DstMemType: The type of destination memory accessed by memory operation/copy
+==19104== Generated result file: /home/paperspace/fast_kinematics/build/results.nvprof
--- tests/test_client.cpp
+++ tests/test_client.cpp
@@ -2,7 +2,6 @@
 #include <parser.h>
 #include <kinematics.h>
 #include <thread>
-#include <chrono>
 
 size_t find_num_of_active_joints(JointTreePtr tip) {
   size_t num_of_active_joints = 0;
@@ -50,48 +49,13 @@ int main() {
     }
   }
 
-  // sanity check do the chain transformation from the tip to the root
-  // making all the joint values to be zero
-  // We start with two quaternions, v representing the position we are at right now, and q representing the rotation
-  // for each joint, we update v and q.
-  // for rotation, we just multiply the quaternion
-  // for the transformation, we have to rotate the position by the quaternion, and then add the translation
-  Eigen::Quaternionf r = Eigen::Quaternionf::Identity();
-  Eigen::Quaternionf t = Eigen::Quaternionf(0, 0, 0, 0);
-  JointTreePtr walker = tip;
-  // size_t num_of_active_joints = find_num_of_active_joints(tip);
-  // std::vector<float> joint_values{0, 0.2, 0, -2.6, 0, 3.0, 0.8, 0.03};
-  // while (walker) {
-  //   if (walker->type == urdf::Joint::REVOLUTE) {
-  //     // convert Eigen::AngleAxisf(joint_values[--num_of_active_joints], walker->axis) to quaternion
-  //     Eigen::Quaternionf change = Eigen::Quaternionf(Eigen::AngleAxisf(joint_values[--num_of_active_joints], walker->axis));
-  //     r = change * r;
-  //     t = change * t * change.inverse();
-  //     std::cout << change.coeffs().transpose() << std::endl;
-  //   } else if (walker->type == urdf::Joint::PRISMATIC) {
-  //     auto translation = joint_values[--num_of_active_joints] * walker->axis;
-  //     Eigen::Quaternionf change = Eigen::Quaternionf(0, translation.x(), translation.y(), translation.z());
-  //     t.coeffs() += change.coeffs();
-  //     std::cout << change.coeffs().transpose() << std::endl;
-  //   }
-  //   std::cout << walker->rotation.coeffs().transpose() << std::endl;
-  //   std::cout << walker->position.coeffs().transpose() << std::endl;
-  //   r = walker->rotation * r;
-  //   t = walker->rotation * t * walker->rotation.inverse();
-  //   t.coeffs() += walker->position.coeffs();
-  //   walker = walker->parent;
-  // }
-  // std::cout << "The transformation from the tip to the root is: " << std::endl;
-  // std::cout << "Rotation: " << r.coeffs().transpose() << std::endl;
-  // std::cout << "Position: " << t.coeffs().transpose() << std::endl;
-
   // data structure
   // [translation (3 floats), rotation (4 floats), type (1 float), axis (3 floats)]
   
-  size_t num_of_robots = 10000;
+  size_t num_of_robots = 100000;
 
   float *h_data = new float[11 * find_num_of_joints(tip) * num_of_robots];
-  walker = tip;
+  JointTreePtr walker = tip;
   size_t idx = 11 * find_num_of_joints(tip) * num_of_robots;
   while (walker) {
     h_data[--idx] = walker->axis.z();
@@ -145,33 +109,25 @@ int main() {
   cudaMalloc(&d_num_of_joints_cum, num_of_robots * sizeof(size_t));
   cudaMalloc(&d_num_of_active_joints_cum, num_of_robots * sizeof(size_t));
 
-  std::cout << "memory assigned" << std::endl;
-  std::chrono::high_resolution_clock::time_point start = std::chrono::high_resolution_clock::now();
-
   cudaMemcpy(d_data, h_data, h_num_of_data_cum[num_of_robots-1] * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_angs, h_angs, h_num_of_active_joints_cum[num_of_robots-1] * sizeof(float), cudaMemcpyHostToDevice);
   cudaMemcpy(d_num_of_joints_cum, h_num_of_data_cum, num_of_robots * sizeof(size_t), cudaMemcpyHostToDevice);
   cudaMemcpy(d_num_of_active_joints_cum, h_num_of_active_joints_cum, num_of_robots * sizeof(size_t), cudaMemcpyHostToDevice);
   cudaMemcpy(d_result, h_result, 6*h_num_of_active_joints_cum[num_of_robots-1] * sizeof(float), cudaMemcpyHostToDevice);
 
-  std::cout << "memory copied" << std::endl;
-
   // jacobian(d_data, d_angs, d_num_of_joints_cum, d_num_of_active_joints_cum, 3, d_result);
-  dim3 grid((num_of_robots+1023)/1024);
-  dim3 block(1024);
+  dim3 grid((num_of_robots+255)/256);
+  dim3 block(256);
   jacobian<<<grid, block>>>(d_data, d_angs, d_num_of_joints_cum, d_num_of_active_joints_cum, d_result, num_of_robots);
 
   cudaMemcpy(h_result, d_result, 6*h_num_of_active_joints_cum[num_of_robots-1] * sizeof(float), cudaMemcpyDeviceToHost);
 
-  std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
-  std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
-  std::cout << "The time to copy data and for the kernel to run on a size 10000 is: " << time_span.count() << " seconds." << std::endl;
-  for (size_t i = 0; i < 6; ++i) {
-    for (size_t j = h_num_of_active_joints_cum[9998]; j < h_num_of_active_joints_cum[9999]; ++j) {
-      std::cout << h_result[j*6+i] << " ";
-    }
-    std::cout << std::endl;
-  }
+  // for (size_t i = 0; i < 6; ++i) {
+  //   for (size_t j = h_num_of_active_joints_cum[9998]; j < h_num_of_active_joints_cum[9999]; ++j) {
+  //     std::cout << h_result[j*6+i] << " ";
+  //   }
+  //   std::cout << std::endl;
+  // }
 
   return 0;
 }
